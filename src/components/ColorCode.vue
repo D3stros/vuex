fe3a379f80fdcee8c4d6185da055259fcccd258b
D3stros <patrick.fallmann@hotmail.com>
2022-08-24 21:49:20 +0200

    Creating child components

---
+++ src/components/ColorCode.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="div">
+    <input
+    v-model="colorCode"
+    placeholder="Enter color code" 
+    type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    colorCode: {
+        get() {
+          return this.$store.state.colorCode
+        },
+        set(newValue) {
+          this.$store.dispatch('setColorCode', newValue)
+        }
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <div 
-    :style="{color: $store.state.colorCode}"
-    class="counter">{{$store.state.counter}}</div>
-    <div class="counter-squared">
-      {{$store.state.counter}}
-      <sup>2</sup> =
-      {{$store.getters.counterSquared}}
-      </div>
-    <div class="buttons">
-      <button @click="$store.dispatch('decreaseCounter')">-</button>
-      <button @click="$store.dispatch('increaseCounter')">+</button>
-    </div>
-  </div>
-  <div class="div">
-    <input
-    v-model="colorCode"
-    placeholder="Enter color code" 
-    type="text" />
+    <counter />
+    <counter-squared />
+    <buttons />
+    <color-code />
   </div>
 </template>
 
@@ -26,15 +12,11 @@
 
 export default {
   name: 'HomeView',
-  computed: {
-    colorCode: {
-        get() {
-          return this.$store.state.colorCode
-        },
-        set(newValue) {
-          this.$store.dispatch('setColorCode', newValue)
-        }
-    }
+  components: {
+    'counter': require('@/components/Counter.vue').default,
+    'counter-squared': require('@/components/CounterSquared.vue').default,
+    'buttons': require('@/components/Buttons.vue').default,
+    'color-code': require('@/components/ColorCode.vue').default
   }
 }
 </script>
